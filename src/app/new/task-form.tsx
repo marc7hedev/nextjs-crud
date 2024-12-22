3e0cfef47ef6ef29b9c97418e911201857ae1441
marc7hedev <driberstagg@gmail.com>
2024-12-22 14:32:54 -0600

    Implementado server action en formulario task-form

--- src/app/new/task-form.tsx
+++ src/app/new/task-form.tsx
@@ -21,50 +21,69 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 
 export function TaskForm() {
+    async function createTask(formData: FormData) {
+        "use server";
+        const name = formData.get("name");
+        const description = formData.get("description");
+        const priority = formData.get("priority");
+
+        console.log({ name, description, priority });
+    }
+
     return (
-        <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>Crear tarea</CardTitle>
-                <CardDescription>
-                    Completa el formulario para crear una nueva tarea
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
+        <form action={createTask}>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Crear tarea</CardTitle>
+                    <CardDescription>
+                        Completa el formulario para crear una nueva tarea
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
                     <div className="grid w-full items-center gap-4">
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">Nombre</Label>
-                            <Input id="name" placeholder="Nombre de la tarea" />
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="description">Descripción</Label>
-                            <Textarea 
-                                name="description"
-                                id="description" 
-                                placeholder="Descripción" 
+                            <Input
+                                name="name"
+                                id="name"
+                                placeholder="Nombre de la tarea"
                             />
                         </div>
                         <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="framework">Prioridad</Label>
-                            <Select>
-                                <SelectTrigger id="framework">
+                            <Label htmlFor="description">Descripción</Label>
+                            <Textarea
+                                name="description"
+                                id="description"
+                                placeholder="Descripción"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="priority">Prioridad</Label>
+                            <Select name="priority">
+                                <SelectTrigger id="priority">
                                     <SelectValue placeholder="Seleccionar" />
                                 </SelectTrigger>
                                 <SelectContent position="popper">
                                     <SelectItem value="low">Baja</SelectItem>
-                                    <SelectItem value="medium">Media</SelectItem>
+                                    <SelectItem value="medium">
+                                        Media
+                                    </SelectItem>
                                     <SelectItem value="high">Alta</SelectItem>
-                                    <SelectItem value="urgent">Urgente</SelectItem>
+                                    <SelectItem value="urgent">
+                                        Urgente
+                                    </SelectItem>
                                 </SelectContent>
                             </Select>
                         </div>
                     </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                <Button variant="outline">Cancelar</Button>
-                <Button>Crear</Button>
-            </CardFooter>
-        </Card>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant="outline">Cancelar</Button>
+                    <Button type="submit">
+                        Crear tarea
+                    </Button>
+                </CardFooter>
+            </Card>
+        </form>
     );
 }

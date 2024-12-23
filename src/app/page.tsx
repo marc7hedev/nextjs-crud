7e65dca0db258ea01b8fba6fcbfe89e098ba0c40
marc7hedev <driberstagg@gmail.com>
2024-12-23 14:02:48 -0600

    Refactorizado componente card en TaskCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
+
 import prisma from "@/lib/prisma";
-import clsx from "clsx";
 
 async function HomePage() {
     const tasks = await prisma.task.findMany();
@@ -17,33 +8,7 @@ async function HomePage() {
     return (
         <div className="grid grid-cols-3 gap-4">
             {tasks.map((task) => (
-                <Card key={task.id}>
-                    <CardHeader className="flex flex-row justify-between">
-                        <CardTitle>
-                            {task.name}
-                        </CardTitle>
-                        <Badge className={
-                        clsx({
-                            'bg-red-700': task.priority === 'urgent',
-                            'bg-red-400': task.priority === 'high',
-                            'bg-yellow-500': task.priority === 'medium',
-                            'bg-green-500': task.priority === 'low',
-                        })
-                        }>
-                            {task.priority}
-                        </Badge>
-                    </CardHeader>
-                    <CardContent>
-                        <p>{task.description}</p>
-                        <span className="text-slate-600">
-                            {new Date(task.createdAt).toLocaleDateString()}
-                        </span>
-                    </CardContent>
-                    <CardFooter className="flex gap-x-2 justify-end">
-                        <Button variant="destructive">Eliminar</Button>
-                        <Button>Editar</Button>
-                    </CardFooter>
-                </Card>
+                
             ))}
         </div>
     );
---
+++ src/components/task-card.tsx
@@ -0,0 +1,43 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Task } from "@prisma/client";
+import clsx from "clsx";
+
+export function TaskCard({task}: {task: Task}) {
+    return (
+        <Card key={task.id}>
+                    <CardHeader className="flex flex-row justify-between">
+                        <CardTitle>
+                            {task.name}
+                        </CardTitle>
+                        <Badge className={
+                            clsx({
+                                'bg-red-700 text-white': task.priority === 'urgent',
+                                'bg-red-400': task.priority === 'high',
+                                'bg-yellow-500': task.priority === 'medium',
+                                'bg-green-500': task.priority === 'low',
+                            })
+                        }>
+                            {task.priority}
+                        </Badge>
+                    </CardHeader>
+                    <CardContent>
+                        <p>{task.description}</p>
+                        <span className="text-slate-600">
+                            {new Date(task.createdAt).toLocaleDateString()}
+                        </span>
+                    </CardContent>
+                    <CardFooter className="flex gap-x-2 justify-end">
+                        <Button variant="destructive">Eliminar</Button>
+                        <Button>Editar</Button>
+                    </CardFooter>
+                </Card>
+    )
+}

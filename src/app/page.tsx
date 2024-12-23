1d63145cdc984ee8016c86c8a65591ec8a03068e
marc7hedev <driberstagg@gmail.com>
2024-12-23 12:26:07 -0600

    Añadido Badge al card de page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
     Card,
@@ -7,6 +8,7 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 import prisma from "@/lib/prisma";
+import clsx from "clsx";
 
 async function HomePage() {
     const tasks = await prisma.task.findMany();
@@ -16,10 +18,20 @@ async function HomePage() {
         <div className="grid grid-cols-3 gap-4">
             {tasks.map((task) => (
                 <Card key={task.id}>
-                    <CardHeader>
+                    <CardHeader className="flex flex-row justify-between">
                         <CardTitle>
                             {task.name}
                         </CardTitle>
+                        <Badge className={
+                        clsx({
+                            'bg-red-700': task.priority === 'urgent',
+                            'bg-red-400': task.priority === 'high',
+                            'bg-yellow-500': task.priority === 'medium',
+                            'bg-green-500': task.priority === 'low',
+                        })
+                        }>
+                            {task.priority}
+                        </Badge>
                     </CardHeader>
                     <CardContent>
                         <p>{task.description}</p>

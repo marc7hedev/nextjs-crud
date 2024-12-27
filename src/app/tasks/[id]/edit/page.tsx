a5a3a72fa5bc30e155bba228c86ab7eabdd5c922
marc7hedev <driberstagg@gmail.com>
2024-12-26 19:53:57 -0600

    Corregido problema de params

--- src/app/tasks/[id]/edit/page.tsx
+++ src/app/tasks/[id]/edit/page.tsx
@@ -2,26 +2,24 @@ import { TaskForm } from "@/app/new/task-form";
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
 
-
-export default async function TaskPageEdit({params}: {
-    params: { 
-        id: string;
-    };
-}) {
+export default async function TaskPageEdit({ params }: { params: Promise<{ id: string }> }) {
+    // Asegúrate de que params se resuelva correctamente
+    const { id } = await params;
+    console.log({ id });
 
     const task = await prisma.task.findFirst({
         where: {
-            id: parseInt(params.id)
-        }
+            id: parseInt(id),
+        },
     });
-    
+
     if (!task) {
         redirect("/");
     }
-    
+
     return (
         <div className="flex justify-center items-center h-screen">
-            <TaskForm task={task}/>
+            <TaskForm task={task} />
         </div>
     );
 }

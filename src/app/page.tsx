60d3ae3e31a1635b6bae116b9fc3e002883cc1c7
marc7hedev <driberstagg@gmail.com>
2024-12-26 20:54:45 -0600

    Agregado metadata de titulos y ajustado contenedor principal para responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 import { TaskCard } from "@/components/task-card";
 import prisma from "@/lib/prisma";
 
+export const metadata = {
+    title: "marc7System",
+    description: "Basic Crud App",
+};
+
+
 async function HomePage() {
     const tasks = await prisma.task.findMany();
     console.log(tasks);
 
     return (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             {tasks.map((task) => (
                 <TaskCard task={task} key={task.id}/>
             ))}
